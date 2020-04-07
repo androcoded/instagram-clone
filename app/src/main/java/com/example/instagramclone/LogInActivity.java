ff9df112dd8eda6103dfbd3a38e6c8534436e79e
dreamdroiddev <dreamdroiddev@gmail.com>
2020-04-08 02:03:53 +0600

    User login

--- app/src/main/java/com/example/instagramclone/LogInActivity.java
+++ app/src/main/java/com/example/instagramclone/LogInActivity.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -32,6 +39,20 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnLogin:
+                ParseUser.logInInBackground(edtLoginUsername.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user!=null && e == null){
+                            FancyToast.makeText(getApplicationContext(),user.getUsername()+ " is successfully logged in!",
+                                    Toast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
+                            Intent intentWelcomeActivity = new Intent(getApplicationContext(),WelcomeActivity.class);
+                            startActivity(intentWelcomeActivity);
+                        }else{
+                            FancyToast.makeText(getApplicationContext(),e.getMessage()+ "",
+                                    Toast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                        }
+                    }
+                });
                 break;
             case R.id.btnSignUpActivity:
                 Intent intentSignUp = new Intent(getApplicationContext(),SignUpActivity.class);
--- app/src/main/java/com/example/instagramclone/WelcomeActivity.java
+++ app/src/main/java/com/example/instagramclone/WelcomeActivity.java
@@ -2,13 +2,34 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private TextView txtWelcomeText;
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        txtWelcomeText = findViewById(R.id.textView);
+        txtWelcomeText.setText("Welcome!");
+        btnLogout = findViewById(R.id.btnLogOut);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                finish();
+            }
+        });
+
+
     }
 }
